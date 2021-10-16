ade5af835addd113d8ebac5039186da378d623cd
pepitox <lavashuriken777@gmail.com>
2021-10-16 12:59:40 -0300

    Mejora la legibilidad
    El código estaba muy desordenado, ahora se puede leer con mas claridad por que esta formateado con la aplicación Pretty.

--- js-xmas-edition-master/js/pruebas.js
+++ js-xmas-edition-master/js/pruebas.js
@@ -1,50 +1,55 @@
-
 function probarValidarNombre() {
   console.assert(
-      validarNombre('') === 'El campo nombre debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
+    validarNombre("") === "El campo nombre debe tener al menos 1 caracter",
+    "Validar nombre no validó que el nombre no sea vacío"
   );
 
   console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+    validarNombre(
+      "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+    ) === "Este campo debe tener menos de 50 caracteres",
+    "Validar nombre no validó que el nombre sea menor a 50 caracteres"
   );
   console.assert(
-      validarNombre("asdf1234") === "El campo nombre solo acepta letras",
-      "validar nombre no valido que el nombre solo tenga letras"
-  )
+    validarNombre("asdf1234") === "El campo nombre solo acepta letras",
+    "validar nombre no valido que el nombre solo tenga letras"
+  );
   console.assert(
-      validarNombre("Fabricio") === "", "validarNombre fallo con un nombre valido"
-  )
+    validarNombre("Fabricio") === "",
+    "validarNombre fallo con un nombre valido"
+  );
 }
 
-function probarValidarCiudad(){ 
-console.assert(
-    validarCiudad("") === 'El campo ciudad no puede estar vacío',
-    'Validar ciudad no validó que la ciudad no este seleccionada',
-)
-console.assert(
-    validarCiudad("Buenos Aires") === "", "Validar ciudad no funciono con un nombre de ciudad valido"
-    )
-
+function probarValidarCiudad() {
+  console.assert(
+    validarCiudad("") === "El campo ciudad no puede estar vacío",
+    "Validar ciudad no validó que la ciudad no este seleccionada"
+  );
+  console.assert(
+    validarCiudad("Buenos Aires") === "",
+    "Validar ciudad no funciono con un nombre de ciudad valido"
+  );
 }
-function probarValidarDescripcionRegalo(){ 
-console.assert(
-    validarDescripcionRegalo("qwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiret") === "El campo descripcion es muy largo", 
+function probarValidarDescripcionRegalo() {
+  console.assert(
+    validarDescripcionRegalo(
+      "qwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiretqwequiret"
+    ) === "El campo descripcion es muy largo",
     "La funcion validar descripcion regalo no valido que el campo no sea muy largo"
-)
+  );
 
-console.assert(
-    validarDescripcionRegalo("Regalo") === "", "La funcion validar descripcion regalo no funciono con una descripcion correcta"
-)
+  console.assert(
+    validarDescripcionRegalo("Regalo") === "",
+    "La funcion validar descripcion regalo no funciono con una descripcion correcta"
+  );
 
-console.assert(
-    validarDescripcionRegalo(".,.,.,.,") === "El campo descripcion solo puede tener numeros y letras",
-    "La funcion validar descripcion no valido que fuera solo numeros y letras")
+  console.assert(
+    validarDescripcionRegalo(".,.,.,.,") ===
+      "El campo descripcion solo puede tener numeros y letras",
+    "La funcion validar descripcion no valido que fuera solo numeros y letras"
+  );
 }
 
-probarValidarNombre()
-probarValidarCiudad()
-probarValidarDescripcionRegalo()
+probarValidarNombre();
+probarValidarCiudad();
+probarValidarDescripcionRegalo();
